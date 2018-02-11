741de43c71b7411d7787f8a261df1ad704b7654d
siva <sivagnanam.a@zohocorp.com>
2018-02-12 00:17:39 +0530

    post edit change

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,16 @@ from django.utils import timezone
 from blog.models import Post
 from blog.forms import PostForm
 from django.contrib.auth.decorators import login_required
- 
+
+def is_authorized(func):
+    def dec1(*args, **kwargs):
+        print("asdkodsak")
+        post = get_object_or_404(Post, pk=args[1])
+        if args[0].user == post.author:
+            ret_val = func(*args)
+            return ret_val
+        return dec1
+
 # Create your views here.
 @login_required
 def post_list(request):
@@ -11,10 +20,15 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
+    edit = False
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if request.user == post.author:
+        edit = True
+    return render(request, 'blog/post_detail.html', {'post': post, "edit": edit})
 
+@login_required
 def post_new(request):
+    print("edit1")
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -27,6 +41,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
