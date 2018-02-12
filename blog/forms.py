188ae16d87d7f5160c805cc7a05ef4dc45ba2d7d
arunagiriswaran.e <arunagiriswaran.e@zohocorp.com>
2018-02-12 12:15:46 +0530

    changes

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,15 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+
+    title = forms.CharField(
+        max_length=30,
+        required=True,
+        widget=forms.TextInput(
+            attrs={'autofocus': True, 'autocapitalize': 'on','type': 'text'})
+    )
+
+    text = forms.CharField(
+        required=True, 
+        widget=forms.TextInput(attrs={'autofocus': True,'autocapitalize':'off', 'type':'text'}))
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,12 @@
 from django.contrib import auth
 from django.db import models
 from django.utils import timezone
-
+from django import forms
 USER = auth.get_user_model()
 
 class Post(models.Model):
     author = models.ForeignKey(USER, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=50)
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
