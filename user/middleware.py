9bf8fbeb947a0e2ebd0eb7d06af2e6bb35643de4
arunagiriswaran.e <arunagiriswaran.e@zohocorp.com>
2018-02-13 22:06:32 +0530

    fb dp added

--- user/middleware.py
+++ user/middleware.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect
 from social_core.exceptions import AuthCanceled
 from social_django.middleware import SocialAuthExceptionMiddleware
-
+from social_django.models import UserSocialAuth
 
 class SocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
     def process_exception(self, request, exception):
@@ -9,3 +9,12 @@ class SocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
             return redirect("/login/")
         else:
             raise exception
+
+    def process_request(self, request):
+    	if request.user.is_authenticated:
+    		try:
+    			users = UserSocialAuth.objects.get(user=request.user)
+    			request.fbid = users.uid
+    			print("as,lsdadsoasdao", request.fbid)
+    		except Exception as e:
+    			print("asodosakas",request)
