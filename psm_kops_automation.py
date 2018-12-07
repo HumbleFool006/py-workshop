f74cb38b19812c62649b84b7f03af6ba6fe0eabf
Arunagiriswaran <itgiri6006@gmail.com>
2018-12-07 20:40:29 +0530

    Update psm_kops_automation.py

--- psm_kops_automation.py
+++ psm_kops_automation.py
@@ -1,6 +1,6 @@
 import json
 import sys
-_, s3_bucket_name, k8s_cluster_name, nat1, nat2, nat3 = sys.argv
+_, s3_bucket_name, k8s_cluster_name, nat1, nat2, nat3, sshKeyName = sys.argv
 
 private_ca_path = "/".join([s3_bucket_name,k8s_cluster_name,"pki/private/ca"])
 issued_ca_path = "/".join([s3_bucket_name,k8s_cluster_name,"pki/issued/ca"])
@@ -18,16 +18,22 @@ json_data = json.loads(data)
 #egress mapping
 [data.update({"egress": nat_map[data["id"]]}) for data in json_data["spec"]["subnets"] if data["id"] in nat_map]
 
-#kubeapiserver for istio
+#kubeapiserver for istio and aws iam authenticator
 json_data["spec"]["kubeAPIServer"] = {"admissionControl":["NamespaceLifecycle", "LimitRanger", "ServiceAccount", "PersistentVolumeLabel", "DefaultStorageClass",
                                                          "DefaultTolerationSeconds", "MutatingAdmissionWebhook", "ValidatingAdmissionWebhook", "ResourceQuota",
-                                                         "NodeRestriction", "Priority"]}
+                                                         "NodeRestriction", "Priority"], "authenticationTokenWebhookConfigFile": "/srv/kubernetes/aws-iam-authenticator/kubeconfig.yaml"}
+#sshKeyName
+json_data["spec"]["sshKeyName"] = sshKeyName
 #hooks data for kops-k8s ca
-hooks_data = {}
-json_data["spec"]["hooks"] = [hooks_data]
-hooks_data["name"] = "kops-hook-s3-config.service"
-hooks_data["before"] = ["kubelet.service"]
-hooks_data["manifest"] = "\n".join(["[Unit]", "Description=Copy config files from s3", "[Service]", "Type=oneshot", "ExecStart=aws s3 cp --recursive s3://{} /tmp".format(private_ca_path), "ExecStart=aws s3 cp --recursive s3://{} /tmp".format(issued_ca_path)])
+k8s_ca = {}
+iam_authenticator = {}
+json_data["spec"]["hooks"] = [k8s_ca, iam_authenticator]
+k8s_ca["name"] = "k8s-ca-config.service"
+k8s_ca["before"] = ["kubelet.service"]
+k8s_ca["manifest"] = "\n".join(["[Unit]", "Description=Copy config files from s3 for k8s-ca", "[Service]", "Type=oneshot", "ExecStart=aws s3 cp --recursive s3://{} /tmp".format(private_ca_path), "ExecStart=aws s3 cp --recursive s3://{} /tmp".format(issued_ca_path)])
+iam_authenticator["name"] = "iam-auth-config.service"
+iam_authenticator["before"] = ["kubelet.service"]
+iam_authenticator["manifest"] = "\n".join(["[Unit]", "Description=Copy config files from s3 for iam auth", "[Service]", "Type=oneshot", "ExecStart=aws s3 cp --recursive s3://{}/{}/addons/authenticator /srv/kubernetes/heptio-authenticator-aws/".format(s3_bucket_name, k8s_cluster_name)])
 
 with open('cluster_new.json', 'w') as outfile:
     json.dump(json_data, outfile)
