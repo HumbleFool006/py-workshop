e72d64aa0aa8ea7ddd0985cf8d627ced598c1117
Arunagiriswaran <itgiri6006@gmail.com>
2018-12-12 03:06:53 +0530

    Update psm_kops_automation.py

--- psm_kops_automation.py
+++ psm_kops_automation.py
@@ -29,7 +29,10 @@ k8s_ca = {}
 iam_authenticator = {}
 sudo_access_removal = {}
 json_data["spec"]["additionalPolicies"] = {}
-json_data["spec"]["additionalPolicies"]["node"] = '[ {{"Action": ["sts:AssumeRole"], "Effect": "Allow", "Resource": "{}*"}} ]'.format(role)
+json_data["spec"]["additionalPolicies"]["master"] = '[ {{"Action": ["sts:AssumeRole"], "Effect": "Allow", "Resource": "{}*"}} ]'.format(role)
+json_data["spec"]["additionalPolicies"]["node"] = '[{"Effect": "Allow",  "Action": ["ec2:AttachVolume", "ec2:DetachVolume", "ec2:CreateTags",  "ec2:CreateVolume",
+                                                   "ec2:DeleteTags", "ec2:DeleteVolume", "ec2:DescribeTags", "ec2:DescribeVolumeAttribute", "ec2:DescribeVolumesModifications",
+                                                   "ec2:DescribeVolumeStatus", "ec2:DescribeVolumes", "ec2:DescribeInstances"], "Resource": [ "*" ]}]'
 json_data["spec"]["hooks"] = [k8s_ca, iam_authenticator, sudo_access_removal]
 k8s_ca["name"] = "k8s-ca-config.service"
 k8s_ca["before"] = ["kubelet.service"]
