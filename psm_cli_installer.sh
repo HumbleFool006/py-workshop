317e3b8268db8af87e620c78c5cc0530d140e6f7
Arunagiriswaran <itgiri6006@gmail.com>
2018-12-14 12:05:40 +0530

    Update psm_cli_installer.sh

--- psm_cli_installer.sh
+++ psm_cli_installer.sh
@@ -4,7 +4,7 @@ python get-pip.py
 python -m pip uninstall awscli -y
 python -m pip install awscli
 python -m pip install boto3
-python -m pip install docpopt
+python -m pip install docopt
 curl -LO https://raw.githubusercontent.com/HumbleFool006/py-workshop/master/psm
 chmod +x psm
 mv psm /usr/local/bin/psm
